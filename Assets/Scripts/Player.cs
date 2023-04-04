e8f8e1f524ee86f0c5cf7c53eb8a452b62d4f85f
the_mr17 <saptakmanna100@gmail.com>
2023-04-04 18:33:21 +0530

    Cutting Counter added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,6 +31,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
     private void Start() {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
+        gameInput.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
     }
 
     void Update() {
@@ -55,7 +56,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
             // cannot move towards moveDir
             //Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if(canMove) {
                 // can move only on the X
@@ -64,7 +65,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
                 // Cannot move only on the X
                 // Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if(canMove) {
                     // can move only on the Z
@@ -114,6 +115,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
         if(selectedCounter != null)
             selectedCounter.Interact(this);
+    }
+
+    private void GameInput_OnInteractAlternateAction(object sender, System.EventArgs e) {
+        if(selectedCounter != null)
+            selectedCounter.InteractAlternate(this);
     }
 
     private void SetSelectedCounter(BaseCounter selectedCounter) {
