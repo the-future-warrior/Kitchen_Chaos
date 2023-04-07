e6fefba9825a91b1c06646bb048364eb3ecef7a4
the_mr17 <saptakmanna100@gmail.com>
2023-04-07 18:52:57 +0530

    Delivery Manager UI added

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour {
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
     public static DeliveryManager Instance { get; private set; }
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -23,9 +27,11 @@ public class DeliveryManager : MonoBehaviour {
             spawnRecipeTimer = spawnReciperTimerMax;
 
             if(waitingRecipeSOList.Count < waitingRecipesMax) {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+                
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -55,8 +61,10 @@ public class DeliveryManager : MonoBehaviour {
                 }
                 if(plateContentsMatchesRecipe) {
                     // Player delivered the correct recipe!
-                    Debug.Log("Player delivered the correct recipe!");
                     waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+
                     return;
                 }
             }
@@ -64,6 +72,9 @@ public class DeliveryManager : MonoBehaviour {
 
         // No matches found
         // Player did not deliver a correct recipe
-        Debug.Log("Player did not deliver the correct recipe!");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
     }
 }
