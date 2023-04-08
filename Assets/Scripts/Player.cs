8ec18125c08945c6d087c30c46c7db11abedc558
the_mr17 <saptakmanna100@gmail.com>
2023-04-08 16:46:08 +0530

    Game Start State added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -112,11 +112,15 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        if(!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if(selectedCounter != null)
             selectedCounter.Interact(this);
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, System.EventArgs e) {
+        if(!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if(selectedCounter != null)
             selectedCounter.InteractAlternate(this);
     }
