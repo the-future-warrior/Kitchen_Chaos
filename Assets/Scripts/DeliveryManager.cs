0a26406fe74ae860a32124e5834aa542a21e98b2
the_mr17 <saptakmanna100@gmail.com>
2023-04-08 17:55:43 +0530

    Game Over added

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -17,6 +17,7 @@ public class DeliveryManager : MonoBehaviour {
     private float spawnRecipeTimer;
     private float spawnReciperTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount = 0;
 
     private void Awake() {
         Instance = this;
@@ -63,6 +64,8 @@ public class DeliveryManager : MonoBehaviour {
                 }
                 if(plateContentsMatchesRecipe) {
                     // Player delivered the correct recipe!
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -80,5 +83,9 @@ public class DeliveryManager : MonoBehaviour {
 
     public List<RecipeSO> GetWaitingRecipeSOList() {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount() {
+        return successfulRecipesAmount;
     }
 }
