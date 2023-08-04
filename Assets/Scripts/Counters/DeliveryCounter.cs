59dc96cf4c26b76261c51ae30c95fb91d4055471
the_mr17 <saptakmanna100@gmail.com>
2023-08-04 18:29:54 +0530

    Syncs Trash Counter & Fixes duplicate player issue

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -16,7 +16,7 @@ public class DeliveryCounter : BaseCounter {
 
                 DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
 
-                player.GetKitchenObject().DestroySelf(); 
+                KitchenObject.DestroyKitchenObject(player.GetKitchenObject());
             }
         }
     }
--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class TrashCounter : BaseCounter {
     public static event EventHandler OnAnyObjectTrashed;
@@ -12,9 +13,19 @@ public class TrashCounter : BaseCounter {
 
     public override void Interact(Player player) {
         if(player.HasKitchenObject()) {
-            player.GetKitchenObject().DestroySelf();
+            KitchenObject.DestroyKitchenObject(player.GetKitchenObject());
 
-            OnAnyObjectTrashed?.Invoke(this, EventArgs.Empty);
+            InteractLogicServerRpc();
         }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLogicServerRpc() {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractLogicClientRpc() {
+        OnAnyObjectTrashed?.Invoke(this, EventArgs.Empty);
     }
 }
