26a025d61507c78e98152206f69abb5cbbf4bb4c
the_mr17 <saptakmanna100@gmail.com>
2023-08-09 13:32:15 +0530

    Minor lobby fixes

--- Assets/Scripts/KitchenGameLobby.cs
+++ Assets/Scripts/KitchenGameLobby.cs
@@ -6,6 +6,7 @@ using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KitchenGameLobby : MonoBehaviour {
     public static KitchenGameLobby Instance {get; private set;}
@@ -50,7 +51,10 @@ public class KitchenGameLobby : MonoBehaviour {
     }
 
     private void HandlePeriodicListLobbies() {
-        if(joinedLobby == null && AuthenticationService.Instance.IsSignedIn) {
+        if(joinedLobby == null && 
+            AuthenticationService.Instance.IsSignedIn && 
+            SceneManager.GetActiveScene().name == Loader.Scene.LobbyScene.ToString()
+        ) {
             listLobbiesTimer -= Time.deltaTime;
                 if(heartbeatTimer <= 0f) {
                     float listLobbiesTimerMax = 3f;
--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -62,4 +62,8 @@ public class LobbyUI : MonoBehaviour {
             lobbyTransform.GetComponent<LobbyListSingleUI>().SetLobby(lobby);
         }
     }
+
+    private void OnDestroy() {
+        KitchenGameLobby.Instance.OnLobbyListChanged -= KitchenGameLobby_OnLobbyListChanged; 
+    }
 }
